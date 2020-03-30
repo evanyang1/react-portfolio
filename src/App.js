9a4bef5bb83751793dfba4135de3b11bd69e3c64
Evan Yang <evanyang@pop-os.localdomain>
2020-03-29 21:42:17 -0700

    hashrouter - hopefully solve deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import Home from './components/views/Home'
 import Portfolio from './components/views/Portfolio'
 import About from './components/views/About'
@@ -8,11 +8,11 @@ import About from './components/views/About'
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Route exact path="/" component={Home}/>
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/about" component={About} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
